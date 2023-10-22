5acab72a05cdd1e9550e75e4965d6b84ced53933
Chaitanya Srinivas <111225128+Chaitanya-Srinivas@users.noreply.github.com>
2023-10-23 00:36:06 +0530

    Update MyChats.js

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -81,7 +81,7 @@ const MyChats = ({ fetchAgain }) => {
         d="flex"
         flexDir="column"
         p={3}
-        bg="#F8F8F8"
+        bg="#d9d9d933"
         w="100%"
         h="100%"
         borderRadius="lg"
@@ -93,7 +93,7 @@ const MyChats = ({ fetchAgain }) => {
               <Box
                 onClick={() => setSelectedChat(chat)}
                 cursor="pointer"
-                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                bg={selectedChat === chat ? "lightblue" : "#E8E8E8"}
                 color={selectedChat === chat ? "white" : "black"}
                 px={3}
                 py={2}
